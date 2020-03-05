12c8edb7fa92d3148215173851c347653102c9cb
Kodzo <zeushias89@gmail.com>
2020-03-05 16:27:26 +0100

    commit expression lambda

---
+++ src/presentation/HelloWorldSwing2.java
@@ -0,0 +1,50 @@
+package presentation;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/*
+ * HelloWorldSwing.java requires no other files. 
+ */
+import javax.swing.*;
+
+public class HelloWorldSwing2 {
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be invoked
+	 * from the event-dispatching thread.
+	 */
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("HelloWorldSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+
+		// button
+		JButton b2;
+		b2 = new JButton("Middle button");
+		b2.setVerticalTextPosition(AbstractButton.BOTTOM);
+		b2.setHorizontalTextPosition(AbstractButton.CENTER);
+		frame.getContentPane().add(b2);
+
+		// expression lambda
+		b2.addActionListener(e -> System.out.println("Action !!!!"));
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+}
